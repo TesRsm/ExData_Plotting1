503c795d46c175a9b74105103e5943566331c0af
LiXiao <fujuamdk7@hotmail.com>
2015-07-12 18:28:35 +0800

    upload

---
+++ plot3.R
@@ -0,0 +1,11 @@
+png("exdata-data-household_power_consumption/plot3.png")
+data<-read.table("exdata-data-household_power_consumption/household_power_consumption.txt",sep=";",header=TRUE,colClasses="character")
+data_<-data[data$Date=="1/2/2007"|data$Date=="2/2/2007",]
+time<-strptime(paste(data_$Date,data_$Time),"%d/%m/%Y %H:%M:%S")
+Sys.setlocale("LC_TIME","English")
+plot(time,data_$Sub_metering_1,xlab="",ylab="Energysub metering",type="n")
+points(time,data_$Sub_metering_1,type="l")
+points(time,data_$Sub_metering_2,type="l",col="red")
+points(time,data_$Sub_metering_3,type="l",col="blue")
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lty=1)
+dev.off()
